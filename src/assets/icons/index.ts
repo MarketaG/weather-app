b07efae987b2a034a8b91b2c4311b4883ee35e05
Marketa Gracova <marketagrac@gmail.com>
2026-01-03 16:28:07 +0100

    feat(icons): add SVG icon components

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { MapPin } from "./MapPin";
+export { CalendarDays } from "./CalendarDays";
+export { ChevronDown } from "./ChevronDown";
+export { MagnifyingGlass } from "./MagnifyingGlass";
+export { Home } from "./Home";
+export { ArrowPath } from "./ArrowPath";
