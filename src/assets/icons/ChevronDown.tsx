2580e59df25ba8ae65e75586ccfe2a7cc03f83dd
Marketa Gracova <marketagrac@gmail.com>
2026-01-05 19:09:12 +0100

    fix(svg): correct JSX SVG attributes and split icon into separate components

--- src/assets/icons/ChevronDown.tsx
+++ src/assets/icons/ChevronDown.tsx
@@ -6,13 +6,13 @@ export function ChevronDown({ className }: IconProps) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="3"
+      strokeWidth="3"
       stroke="currentColor"
       className={className}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m19.5 8.25-7.5 7.5-7.5-7.5"
       />
     </svg>
